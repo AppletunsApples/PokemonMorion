941a689f0628eeb81760f122ab78f780da1887e8
RosieTheHedgie <95107150+AppletunsApples@users.noreply.github.com>
2026-01-08 12:33:28 -0500

    Finishing the starter crests

--- Plugins/Custom Scripts/[002] Moves/[000] Custom Moves.rb
+++ Plugins/Custom Scripts/[002] Moves/[000] Custom Moves.rb
@@ -16,27 +16,3 @@ class Battle::Move::LowerUserSpAtkSpDef1 < Battle::Move::StatDownMove
     @statDown = [:SPECIAL_ATTACK, 1, :SPECIAL_DEFENSE, 1]
   end
 end
-
-# Covet, Thief
-class Battle::Move::UserTakesTargetItem < Battle::Move
-  def pbEffectAfterAllHits(user, target)
-    return if user.wild?   # Wild Pokémon can't thieve
-    return if user.fainted?
-    return if target.damageState.unaffected || target.damageState.substitute
-    return if !target.item
-    return if target.unlosableItem?(target.item)
-    return if user.unlosableItem?(target.item)
-    return if target.hasActiveAbility?(:STICKYHOLD) && !@battle.moldBreaker
-    itemName = target.itemName
-    user.item = target.item
-    # Permanently steal the item from wild Pokémon
-    if target.wild? && target.item == target.initialItem
-      $bag.add(target.item)
-      target.pbRemoveItem
-    else
-      target.pbRemoveItem(false)
-    end
-    @battle.pbDisplay(_INTL("{1} stole {2}'s item {3} and sent it to {4}'s bag.", user.pbThis, target.pbThis(true), itemName, $player.name{5}))
-    user.pbHeldItemTriggerCheck
-  end.name
-end
--- Plugins/Custom Scripts/[004] Items/[001] Crest-likes.rb
+++ Plugins/Custom Scripts/[004] Items/[001] Crest-likes.rb
@@ -3,7 +3,36 @@ Battle::ItemEffects::DamageCalcFromUser.add(:DECIDUEYEQUILL,
     next unless user&.pokemon
     next unless user.item == item
     next unless user.pokemon.isSpecies?(:DECIDUEYE)
-    next unless move&.pbContactMove?(user)
-    mults[:power_multiplier] *= 1.3
+    mults[:power_multiplier] *= 1.3 unless move.contactMove?
+  }
+)
+
+class Battle::Move::RecoilMove < Battle::Move
+  def recoilMove?;                  return true; end
+  def pbRecoilDamage(user, target); return 1;    end
+
+  def pbEffectAfterAllHits(user, target)
+    return if target.damageState.unaffected
+    return if !user.takesIndirectDamage?
+    return if user.hasActiveAbility?(:ROCKHEAD)
+    return if user.pokemon.isSpecies?(:EMBOAR) && user.item == :EMBOARPUFF
+    amt = pbRecoilDamage(user, target)
+    amt = 1 if amt < 1
+    if user.pokemon.isSpecies?(:BASCULIN) && [2, 3].include?(user.pokemon.form)
+      user.pokemon.evolution_counter += amt
+    end
+    user.pbReduceHP(amt, false)
+    @battle.pbDisplay(_INTL("{1} is damaged by recoil!", user.pbThis))
+    user.pbItemHPHealCheck
+  end
+end
+
+Battle::ItemEffects::DamageCalcFromUser.add(:INTELEONSCALE,
+  proc { |item, user, target, move, mults, power|
+    next unless user&.pokemon
+    next unless user.item == item
+    next unless user.pokemon.isSpecies?(:INTELEON)
+    next unless move
+    user.pokemon.types = [move.type]3
   }
 )
