25be1680add0d1e8f630aec2732792d874ec1abc
RosieTheHedgie <95107150+AppletunsApples@users.noreply.github.com>
2026-01-10 14:33:15 -0500

    Update [001] Crest-likes.rb

--- Plugins/Custom Scripts/[004] Items/[001] Crest-likes.rb
+++ Plugins/Custom Scripts/[004] Items/[001] Crest-likes.rb
@@ -1,4 +1,4 @@
-Battle::ItemEffects::DamageCalcFromUser.add(:DECIDUEYEQUILL,
+Battle::ItemEffects::DamageCalcFromUser.add(:DECIDUEYESCARF,
   proc { |item, user, target, move, mults, power|
     next unless user.isSpecies?(:DECIDUEYE)
     mults[:power_multiplier] *= 1.3 unless move.contactMove?
@@ -13,7 +13,7 @@ class Battle::Move::RecoilMove < Battle::Move
     return if target.damageState.unaffected
     return if !user.takesIndirectDamage?
     return if user.hasActiveAbility?(:ROCKHEAD)
-    return if user.pokemon.isSpecies?(:EMBOAR) && user.item == :EMBOARPUFF
+    return if user.pokemon.isSpecies?(:EMBOAR) && user.item == :EMBOARSCARF
     amt = pbRecoilDamage(user, target)
     amt = 1 if amt < 1
     if user.pokemon.isSpecies?(:BASCULIN) && [2, 3].include?(user.pokemon.form)
@@ -25,7 +25,7 @@ class Battle::Move::RecoilMove < Battle::Move
   end
 end
 
-Battle::ItemEffects::ModifyMoveBaseType.add(:INTELEONSCALE,
+Battle::ItemEffects::ModifyMoveBaseType.add(:INTELEONSCARF,
   proc { |item, user, move, type|
     next unless user.isSpecies?(:INTELEON)
     next if move.callsAnotherMove? || move.snatched
@@ -39,7 +39,7 @@ Battle::ItemEffects::ModifyMoveBaseType.add(:INTELEONSCALE,
   }
 )
 
-Battle::ItemEffects::ModifyMoveBaseType.add(:MORPEKOFUR,
+Battle::ItemEffects::ModifyMoveBaseType.add(:MORPEKOSCARF,
   proc { |item, user, move, type|
     next unless user.isSpecies?(:MORPEKO)
     next if move.type != :NORMAL
@@ -51,9 +51,36 @@ Battle::ItemEffects::ModifyMoveBaseType.add(:MORPEKOFUR,
 )
 
 # Wurmple Evolutions
-  def pbSpeed
+  Aight I see. Since you have it defined as "Crest-like items I suppose you plan to add more later. In that case tdef pbSpeed
     return 1 if fainted?
-    speed = stat_with_stages(:SPEED)
+    speed_stat = :SPEED
+
+    if itemActive?
+      case self.species
+      when :BEAUTIFLY
+        speed_stat = :SPECIAL_ATTACK if self.item == :BEAUTIFLYSCALE
+      when :DUSTOX
+        speed_stat = :SPECIAL_DEFENSE if self.item == :DUSTOXSCALE
+      end
+    end
+
+      # Base stat
+    base_speed = case speed_stat
+          when :SPECIAL_ATTACK  then @spatk
+          when :SPECIAL_DEFENSE then @spdef
+          else                      @speed
+          end
+    stage = @stages[speed_stat] + STAT_STAGE_MAXIMUM
+    speed = base_speed * STAT_STAGE_MULTIPLIERS[stage] / STAT_STAGE_DIVISORS[stage]
+    speedMult = 1.0    ```
+
+Haven't tested it but it should let you just add the species, with the crest item and what stat it will increase, and then latter at "base_speed" you just define what stat will utilize
+
+Keep in mind, as you wanted, this will make the speed stat for that pokemon fully irrelevant, as all stat changes to the speed will just be ignored. Paralyzis, Tail Wind, abilities and held items still affect it tho, as those are calculated later, but stat stages are ignored.
+
+Similarly, items and effects that increase/decrease spAtk and SpDef don't affect speed. If you want to you'll need to modify the code further
+
+...or maybe they do, don't have time to test the code sadly, but I guess this should do the trick
     speedMult = 1.0
     # Ability effects that alter calculated Speed
     if abilityActive?
@@ -90,14 +117,14 @@ Battle::ItemEffects::ModifyMoveBaseType.add(:MORPEKOFUR,
     return [(speed * speedMult).round, 1].max
   end
 
-Battle::ItemEffects::AccuracyCalcFromUser.add(:DUSTOXSCALE,
+Battle::ItemEffects::AccuracyCalcFromUser.add(:DUSTOXSCARF,
   proc { |ability, user, target, move, modifiers|
     next unless user.isSpecies?(:DUSTOX)
     modifiers[:accuracy_multiplier] *= 1.3
   }
 )
 
-Battle::ItemEffects::AccuracyCalcFromUser.add(:BEAUTIFLYSCALE,
+Battle::ItemEffects::AccuracyCalcFromUser.add(:BEAUTIFLYSCARF,
   proc { |ability, user, target, move, modifiers|
     next unless user.isSpecies?(:BEAUTIFLY)
     modifiers[:accuracy_multiplier] *= 1.3
